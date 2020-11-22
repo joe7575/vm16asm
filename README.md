9aacd7bf9372fb88d0955ae6fc2d29896eb03ba8
Joachim Stolberg <joe.stolberg@gmx.de>
2020-11-22 18:42:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# vm16asm - Assembler for the VM16 CPU
+
+tbd
+
+
+## Installation
+
+
+
+
+## Using
+
+
+
+## License
+
+Copyright (C) 2019-2020 Joachim Stolberg  
+Licensed under the GNU GPLv3   (See LICENSE.txt)
+
+
+
+## History
+
+- 2020-11-21  v1.0.1  * First commit
+
+
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vm16asm",
+    version="1.0.1",
+    author="Joe",
+    author_email="iauit@gmx.de",
+    description="Assembler for the VM16 CPU",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joe7575/vm16asm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+    entry_points={
+        'console_scripts': [
+            'vm16asm=vm16asm:assembler',
+        ],
+    },
+)
+
+
+
+
+
+
+
+
+
